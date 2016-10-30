06482c256fbe0eedbd0bedf3ffa4e271707fe319
Romain ROUSSEL <rorousse@e1r12p2.42.fr>
2016-10-30 13:36:31 +0100

    normage

--- sources/player.c
+++ sources/player.c
@@ -13,6 +13,27 @@
 #include "filler.h"
 #include "../libft/libft.h"
 
+static void	init_algo(t_coos *curr_play, int *i, int *j)
+{
+	*i = 0;
+	*j = 0;
+	curr_play->x = -1;
+	curr_play->y = -1;
+}
+
+static int	print_answer(t_coos curr_play)
+{
+	if (curr_play.x != -1)
+	{
+		ft_putnbr(curr_play.y);
+		ft_putchar(' ');
+		ft_putnbr(curr_play.x);
+		ft_putchar('\n');
+		return (1);
+	}
+	return (0);
+}
+
 static void	find_last_play(t_game *partie)
 {
 	int			i;
@@ -46,10 +67,7 @@ int			algo_naif(t_game *partie)
 	t_coos			curr_play;
 	unsigned int	dist_rel;
 
-	i = 0;
-	j = 0;
-	curr_play.x = -1;
-	curr_play.y = -1;
+	init_algo(&curr_play, &i, &j);
 	dist_rel = partie->hauteur + partie->largeur;
 	find_last_play(partie);
 	while (i < partie->hauteur)
@@ -72,13 +90,6 @@ int			algo_naif(t_game *partie)
 		}
 		i++;
 	}
-	if (curr_play.x != -1)
-	{
-		ft_putnbr(curr_play.y);
-		ft_putchar(' ');
-		ft_putnbr(curr_play.x);
-		ft_putchar('\n');
-		return (1);
-	}
-	return (0);
+	print_answer(curr_play);
+	return (curr_play.x == -1) ?  0 : 1;
 }
